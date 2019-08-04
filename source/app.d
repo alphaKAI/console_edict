3eb76a412e146f554723469776f9c60e66ad1852
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2019-08-05 02:04:27 +0900

    fix bug

--- source/app.d
+++ source/app.d
@@ -8,54 +8,13 @@ import leveldb;
 
 enum SETTING_FILE = "settings.json";
 
-void main() {
-  Config conf = new Config(File(SETTING_FILE, "r"));
-  ParseResult[] results;
-  StopWatch sw;
+struct Context {
+  Config conf;
+  DB did_you_mean_db;
+}
 
-  if (!conf.dict.ready) {
-    writeln("reading and parsing file...");
-
-    sw.start;
-    foreach (file_name, parser; conf.filename_and_parser_map) {
-      results ~= parser.parseFile(File(file_name, "r"));
-    }
-    sw.stop;
-    writefln("Read and parse file is completed [%s]", sw.peek);
-    sw.reset;
-  }
-
-  if (!conf.dict.ready) {
-    writeln("building dictionary.... Internal Dictionary Driver : ", conf.dict);
-    sw.start;
-    foreach (ParseResult result; results) {
-      conf.dict.insert(result.head, result.desc);
-    }
-    sw.stop;
-
-    writefln("Prepare completed [%s]", sw.peek);
-    sw.reset;
-  }
-
-  //wstring[][wstring] did_you_mean_cache
-  auto opt = new Options;
-  opt.create_if_missing = true;
-  enum DID_YOU_MEAN_DB_NAME = "DID_YOU_MEAN_CHACHE_DB";
-  auto did_you_mean_db = new DB(opt, DID_YOU_MEAN_DB_NAME);
-
-  for (;;) {
-    write("WORD > ");
-    wstring input = readln!wstring.chomp;
-
-    if (input == ":q") {
-      writeln("EXIT");
-      break;
-    }
-    if (input == ":words") {
-      writefln("%d words registered", results.length);
-      continue;
-    }
-
+void lookup_mean(ref Context ctx, wstring input) {
+  with (ctx) {
     auto mean = conf.dict.get(input);
     if (!mean.isNull) {
       writefln("<found the word> [%s]", input);
@@ -91,5 +50,58 @@ void main() {
         did_you_mean_db.put(input.to!string, did_you_mean_array.join(SPECIAL_SEP).to!string);
       }
     }
+  }
+}
+
+void main() {
+  Config conf = new Config(File(SETTING_FILE, "r"));
+  ParseResult[] results;
+  StopWatch sw;
+
+  if (!conf.dict.ready) {
+    writeln("reading and parsing file...");
+
+    sw.start;
+    foreach (file_name, parser; conf.filename_and_parser_map) {
+      results ~= parser.parseFile(File(file_name, "r"));
+    }
+    sw.stop;
+    writefln("Read and parse file is completed [%s]", sw.peek);
+    sw.reset;
+  }
+
+  if (!conf.dict.ready) {
+    writeln("building dictionary.... Internal Dictionary Driver : ", conf.dict);
+    sw.start;
+    foreach (ParseResult result; results) {
+      conf.dict.insert(result.head, result.desc);
+    }
+    sw.stop;
+
+    writefln("Prepare completed [%s]", sw.peek);
+    sw.reset;
+  }
+
+  auto opt = new Options;
+  opt.create_if_missing = true;
+  enum DID_YOU_MEAN_DB_NAME = "DID_YOU_MEAN_CHACHE_DB";
+  auto did_you_mean_db = new DB(opt, DID_YOU_MEAN_DB_NAME);
+
+  auto ctx = Context(conf, did_you_mean_db);
+
+  for (;;) {
+    write("WORD > ");
+    wstring input = readln!wstring.chomp;
+
+    if (input == ":q") {
+      writeln("EXIT");
+      break;
+    }
+    if (input == ":words") {
+      writefln("%d words registered", conf.dict.getHeads.length);
+      continue;
+    }
+
+    ctx.lookup_mean(input);
   }
 }
